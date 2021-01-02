f38c7cc5a29e4fe74375e575fb7905ca941560f8
Matt Brown <matthewbrown0798@gmail.com>
2021-01-02 18:15:33 -0500

    removed placeholders

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,7 +9,7 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
+  title: 'Hi my name is, ',
   name: 'Matthew Brown',
   subtitle: 'I am a JS Developer',
   cta: '',
